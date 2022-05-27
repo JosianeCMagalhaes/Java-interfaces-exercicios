239d138bf298eb8745ff2efae2df694b234cfe8f
JosianeMagalhaes <62856269+JosiMglh@users.noreply.github.com>
2022-05-27 03:40:33 -0300

    add Java interfaces exercicios

---
+++ Repeticao01/src/repeticao01/Repeticao01.java
@@ -0,0 +1,21 @@
+package repeticao01;
+
+/**
+ *
+ * @author JOSIANEMAGALHAES
+ */
+public class Repeticao01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int cc = 0;
+        do {
+            System.out.println("Cambalhota");
+            cc++;
+        } while (cc<4);
+    }
+    
+}
